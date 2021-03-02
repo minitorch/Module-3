7d46236e9c3bfb17f8c9234530aa9daf099c7abf
Mamadou Mountagha Bah <bahm1501@dinf-2014-60b.DInf.fsci.usherbrooke.ca>
2021-03-02 09:55:25 -0500

    debug parallelization

--- minitorch/fast_ops.py
+++ minitorch/fast_ops.py
@@ -41,13 +41,13 @@ def tensor_map(fn):
     def _map(out, out_shape, out_strides, in_storage, in_shape, in_strides):
         # TODO: Implement for Task 3.1.
         size = np.prod(out_shape)  # get the size of the out array
-        in_index = np.zeros(MAX_DIMS, np.int32)
-        out_index = np.zeros(MAX_DIMS, np.int32)
+        in_index = np.zeros((size, MAX_DIMS), np.int32)
+        out_index = np.zeros((size, MAX_DIMS), np.int32)
         for i in prange(size):
-            count(i, out_shape, out_index)
-            broadcast_index(out_index, out_shape, in_shape, in_index)
-            k = index_to_position(in_index, in_strides)
-            j = index_to_position(out_index, out_strides)
+            count(i, out_shape, out_index[i])
+            broadcast_index(out_index[i], out_shape, in_shape, in_index[i])
+            k = index_to_position(in_index[i], in_strides)
+            j = index_to_position(out_index[i], out_strides)
             out[j] = fn(in_storage[k])
 
     return njit(parallel=True)(_map)
@@ -119,16 +119,16 @@ def tensor_zip(fn):
     ):
         # TODO: Implement for Task 3.1.
         size = np.prod(out_shape)
-        a_index = np.zeros(MAX_DIMS, np.int32)
-        b_index = np.zeros(MAX_DIMS, np.int32)
-        out_index = np.zeros(MAX_DIMS, np.int32)
+        a_index = np.zeros((size, MAX_DIMS), np.int32)
+        b_index = np.zeros((size, MAX_DIMS), np.int32)
+        out_index = np.zeros(size, MAX_DIMS), np.int32)
         for i in prange(size):
-            count(i, out_shape, out_index)
-            o = index_to_position(out_index, out_strides)
-            broadcast_index(out_index, out_shape, a_shape, a_index)
-            j = index_to_position(a_index, a_strides)
-            broadcast_index(out_index, out_shape, b_shape, b_index)
-            k = index_to_position(b_index, b_strides)
+            count(i, out_shape, out_index[i])
+            o = index_to_position(out_index[i], out_strides)
+            broadcast_index(out_index[i], out_shape, a_shape, a_index[i])
+            j = index_to_position(a_index[i], a_strides)
+            broadcast_index(out_index[i], out_shape, b_shape, b_index[i])
+            k = index_to_position(b_index[i], b_strides)
             out[o] = fn(a_storage[j], b_storage[k]) 
 
     return njit(parallel=True)(_zip)
@@ -191,18 +191,18 @@ def tensor_reduce(fn):
         reduce_size,
     ):
         # TODO: Implement for Task 3.1.
-        out_index = np.zeros(MAX_DIMS, np.int32)
+        out_index = np.zeros((len(out), MAX_DIMS), np.int32)
         a_index = np.zeros(MAX_DIMS, np.int32)
-        for i in range(len(out)):
-            count(i, out_shape, out_index)
-            o = index_to_position(out_index, out_strides)
+        for i in prange(len(out)):
+            count(i, out_shape, out_index[i])
+            o = index_to_position(out_index[i], out_strides)
 
             for s in range(reduce_size):
                 count(s, reduce_shape, a_index)
-                for k in prange(len(reduce_shape)):
+                for k in range(len(reduce_shape)):
                     if reduce_shape[k] != 1:
-                        out_index[k] = a_index[k]
-                j = index_to_position(out_index, a_strides)
+                        out_index[i][k] = a_index[k]
+                j = index_to_position(out_index[i], a_strides)
                 out[o] = fn(out[o], a_storage[j])
 
     return njit(parallel=True)(_reduce)
